b6884b83edcc665c6e00cb249261b0668074fde1
leuti <daniel.leutwyler@gmx.ch>
2019-10-20 13:10:40 +0200

    191020-2 GET returning now part & wayp separately

--- track/model.js
+++ track/model.js
@@ -12,19 +12,67 @@ function getAllRecs(usrId, options) {
     return new Promise((resolve, reject) => {
 
         // Select track main data
-        let query = `SELECT t.trkId, t.trkTrackName, t.trkRoute, t.trkDateBegin, t.trkTypeFid,
-            t.trkSubtypeFid, t.trkOrg, t.trkEvent, t.trkRemarks, t.trkDistance, t.trkTimeOverall, 
-            t.trkTimeToPeak, t.trkTimeToFinish, t.trkGrade, t.trkMeterUp, t.trkMeterDown, t.trkCountry,
-            GROUP_CONCAT(CONCAT(p.prtFirstName, ' ', p.prtLastName)  SEPARATOR ', ') as participants, 
-            GROUP_CONCAT(wayp.waypNameLong SEPARATOR ', ') as peaks 
-        FROM tbl_tracks t
-        JOIN tbl_track_part tp ON t.trkId = tp.trpaTrkId
-        JOIN tbl_part p ON tp.trpaPartId = p.prtId
-        JOIN tbl_track_wayp tw ON t.trkId = tw.trwpTrkId
-        JOIN tbl_waypoints wayp ON tw.trwpWaypID = wayp.waypID
-        WHERE t.trkUsrId = ?
-        GROUP BY t.trkId 
-        LIMIT 20`;
+        let query = `
+            SELECT 
+            t.trkId AS id, 
+                t.trkTrackName AS name, 
+                t.trkRoute AS route , 
+                t.trkDateBegin AS dateBegin, 
+                typ.typName AS type,
+                styp.typName AS subtype, 
+                t.trkOrg AS org, 
+                t.trkEvent AS event, 
+                t.trkRemarks AS remarks, 
+                t.trkDistance AS distance, 
+                t.trkTimeOverall AS timeOverall, 
+                t.trkTimeToPeak AS timeToPeak, 
+                t.trkTimeToFinish AS timeToFinish, 
+                t.trkGrade AS grade, 
+                t.trkMeterUp AS meterUp, 
+                t.trkMeterDown AS meterDown, 
+                t.trkCountry AS country, 
+                GROUP_CONCAT(DISTINCT part.part SEPARATOR ', ') AS participants, 
+                GROUP_CONCAT(DISTINCT huts.waypNameLong SEPARATOR ', ') AS huts,
+                GROUP_CONCAT(DISTINCT peaks.waypNameLong SEPARATOR ', ') AS peaks,
+                GROUP_CONCAT(DISTINCT wpt.waypNameLong SEPARATOR ', ') AS waypoints 
+            FROM tbl_tracks t
+            JOIN tbl_types typ ON t.trkTypeFid = typ.typId
+            JOIN tbl_types styp ON t.trkSubtypeFid = styp.typId
+            LEFT OUTER JOIN (
+                SELECT tp.trpaTrkId, CONCAT(p.prtFirstName, ' ', p.prtLastName) AS part
+                FROM tbl_track_part tp 
+                JOIN tbl_part p ON tp.trpaPartId = p.prtId
+                ) AS part
+            ON t.trkId = part.trpaTrkId
+            LEFT OUTER JOIN (
+                SELECT tw.trwpTrkId, wayp.waypNameLong
+                FROM tbl_track_wayp tw 
+                JOIN tbl_waypoints wayp ON tw.trwpWaypId = wayp.waypID
+                JOIN tbl_types typ ON wayp.waypTypeFid = typ.typId
+                WHERE typ.typCode = 'hu'
+                ) AS huts
+            ON t.trkId = huts.trwpTrkId
+            LEFT OUTER JOIN (
+                SELECT tw.trwpTrkId, wayp.waypNameLong
+                FROM tbl_track_wayp tw 
+                JOIN tbl_waypoints wayp ON tw.trwpWaypId = wayp.waypID
+                JOIN tbl_types typ ON wayp.waypTypeFid = typ.typId
+                WHERE typ.typCode = 'gi'
+                ) AS peaks
+            ON t.trkId = peaks.trwpTrkId
+            LEFT OUTER JOIN (
+                SELECT tw.trwpTrkId, wayp.waypNameLong
+                FROM tbl_track_wayp tw 
+                JOIN tbl_waypoints wayp ON tw.trwpWaypId = wayp.waypID
+                JOIN tbl_types typ ON wayp.waypTypeFid = typ.typId
+                WHERE typ.typCode not in ('hu','gi')
+                ) AS wpt
+            ON t.trkId = wpt.trwpTrkId
+            WHERE trkUsrId = ?
+            GROUP BY t.trkId, t.trkTrackName, t.trkRoute, t.trkDateBegin, typ.typName,
+                styp.typName, t.trkOrg, t.trkEvent, t.trkRemarks, t.trkDistance, t.trkTimeOverall, 
+                t.trkTimeToPeak, t.trkTimeToFinish, t.trkGrade, t.trkMeterUp, t.trkMeterDown, t.trkCountry
+            `;
         
         if (options.sort && ['asc', 'desc'].includes(options.sort.toLowerCase())) {
             //query += ' ORDER BY title ' + options.sort;                   // Sorting needs to be implemented with GRID
@@ -43,17 +91,67 @@ function getAllRecs(usrId, options) {
 
 function getOne(id, usrId) {
     return new Promise((resolve, reject) => {
-        const query = `SELECT t.trkId, t.trkTrackName, t.trkRoute, t.trkDateBegin, t.trkTypeFid,
-            t.trkSubtypeFid, t.trkOrg, t.trkEvent, t.trkRemarks, t.trkDistance, t.trkTimeOverall, 
-            t.trkTimeToPeak, t.trkTimeToFinish, t.trkGrade, t.trkMeterUp, t.trkMeterDown, t.trkCountry,
-            GROUP_CONCAT(CONCAT(p.prtFirstName, ' ', p.prtLastName)  SEPARATOR ', ') as participants, 
-            GROUP_CONCAT(wayp.waypNameLong SEPARATOR ', ') as peaks 
-        FROM tbl_tracks t
-        JOIN tbl_track_part tp ON t.trkId = tp.trpaTrkId
-        JOIN tbl_part p ON tp.trpaPartId = p.prtId
-        JOIN tbl_track_wayp tw ON t.trkId = tw.trwpTrkId
-        JOIN tbl_waypoints wayp ON tw.trwpWaypID = wayp.waypID
-        WHERE t.trkId = ? AND t.trkUsrId = ?`; 
+        const query = `
+            SELECT 
+                t.trkId AS id, 
+                t.trkTrackName AS name, 
+                t.trkRoute AS route , 
+                t.trkDateBegin AS dateBegin, 
+                typ.typName AS type,
+                styp.typName AS subtype, 
+                t.trkOrg AS org, 
+                t.trkEvent AS event, 
+                t.trkRemarks AS remarks, 
+                t.trkDistance AS distance, 
+                t.trkTimeOverall AS timeOverall, 
+                t.trkTimeToPeak AS timeToPeak, 
+                t.trkTimeToFinish AS timeToFinish, 
+                t.trkGrade AS grade, 
+                t.trkMeterUp AS meterUp, 
+                t.trkMeterDown AS meterDown, 
+                t.trkCountry AS country, 
+                GROUP_CONCAT(DISTINCT part.part SEPARATOR ', ') AS participants, 
+                GROUP_CONCAT(DISTINCT huts.waypNameLong SEPARATOR ', ') AS huts,
+                GROUP_CONCAT(DISTINCT peaks.waypNameLong SEPARATOR ', ') AS peaks,
+                GROUP_CONCAT(DISTINCT wpt.waypNameLong SEPARATOR ', ') AS waypoints 
+            FROM tbl_tracks t
+            JOIN tbl_types typ ON t.trkTypeFid = typ.typId
+            JOIN tbl_types styp ON t.trkSubtypeFid = styp.typId
+            LEFT OUTER JOIN (
+                SELECT tp.trpaTrkId, CONCAT(p.prtFirstName, ' ', p.prtLastName) AS part
+                FROM tbl_track_part tp 
+                JOIN tbl_part p ON tp.trpaPartId = p.prtId
+                ) AS part
+            ON t.trkId = part.trpaTrkId
+            LEFT OUTER JOIN (
+                SELECT tw.trwpTrkId, wayp.waypNameLong
+                FROM tbl_track_wayp tw 
+                JOIN tbl_waypoints wayp ON tw.trwpWaypId = wayp.waypID
+                JOIN tbl_types typ ON wayp.waypTypeFid = typ.typId
+                WHERE typ.typCode = 'hu'
+                ) AS huts
+            ON t.trkId = huts.trwpTrkId
+            LEFT OUTER JOIN (
+                SELECT tw.trwpTrkId, wayp.waypNameLong
+                FROM tbl_track_wayp tw 
+                JOIN tbl_waypoints wayp ON tw.trwpWaypId = wayp.waypID
+                JOIN tbl_types typ ON wayp.waypTypeFid = typ.typId
+                WHERE typ.typCode = 'gi'
+                ) AS peaks
+            ON t.trkId = peaks.trwpTrkId
+            LEFT OUTER JOIN (
+                SELECT tw.trwpTrkId, wayp.waypNameLong
+                FROM tbl_track_wayp tw 
+                JOIN tbl_waypoints wayp ON tw.trwpWaypId = wayp.waypID
+                JOIN tbl_types typ ON wayp.waypTypeFid = typ.typId
+                WHERE typ.typCode not in ('hu','gi')
+                ) AS wpt
+            ON t.trkId = wpt.trwpTrkId
+            WHERE t.trkId = ? AND trkUsrId = ?
+            GROUP BY t.trkId, t.trkTrackName, t.trkRoute, t.trkDateBegin, typ.typName,
+                styp.typName, t.trkOrg, t.trkEvent, t.trkRemarks, t.trkDistance, t.trkTimeOverall, 
+                t.trkTimeToPeak, t.trkTimeToFinish, t.trkGrade, t.trkMeterUp, t.trkMeterDown, t.trkCountry
+            `; 
         connection.query(query, [id, usrId], (error, results) => {
             if (error) {
                 console.log(error);
